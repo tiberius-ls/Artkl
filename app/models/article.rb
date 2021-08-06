551939c96d093687c472bfe543d02cee55bab2b8
tiberius-ls <ogbonnaemeka543@gmail.com>
2021-08-06 13:30:21 +0100

    updated user model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
+    belongs_to :user, class_name: "user", foreign_key: "user_id"
     validates :title, presence: true, length: {minimum:3, maximum:50}
     validates :description, presence: true, length: {minimum:10, maximum:10000}
 end
---
+++ db/migrate/20210805135504_ad_user_id_to_articles.rb
@@ -0,0 +1,5 @@
+class AdUserIdToArticles < ActiveRecord::Migration[6.1]
+  def change
+    add_column :articles, :user_id, :integer
+  end
+end
